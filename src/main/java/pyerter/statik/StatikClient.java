e65ff025d971645c7b8009b88e7a1d7ffe5f5282
Porter Squires <PyerterSquires@gmail.com>
2023-02-18 14:19:05 -0500

    Initial Setup

---
+++ src/main/java/pyerter/statik/StatikClient.java
@@ -0,0 +1,11 @@
+package pyerter.statik;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StatikClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
