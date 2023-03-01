d158c844fe8c10809045dd761e928502773c2c2c
Porter Squires <PyerterSquires@gmail.com>
2023-02-28 21:40:53 -0500

    Update REIStatikClientPlugin.java

--- src/main/java/pyerter/statik/integration/rei/REIStatikClientPlugin.java
+++ src/main/java/pyerter/statik/integration/rei/REIStatikClientPlugin.java
@@ -3,6 +3,7 @@ package pyerter.statik.integration.rei;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.client.registry.entry.EntryRegistry;
 import pyerter.statik.integration.rei.category.EngineeringStationRefineRecipeCategory;
 import pyerter.statik.integration.rei.category.TridiRecipeCategory;
 import pyerter.statik.integration.rei.display.EngineeringStationRefineRecipeDisplay;
@@ -26,5 +27,14 @@ public class REIStatikClientPlugin implements REIClientPlugin {
         registry.registerRecipeFiller(TridiRecipe.class, TridiRecipe.Type.INSTANCE, TridiRecipeDisplay::new);
         registry.registerRecipeFiller(EngineeringStationRefineRecipe.class, EngineeringStationRefineRecipe.Type.INSTANCE, EngineeringStationRefineRecipeDisplay::new);
 
+        // TODO: Add display for generated items i.e. Engineered Diamond Sword and whatnot.
+        //registry.registerFiller()
+    }
+
+    @Override
+    public void registerEntries(EntryRegistry registry) {
+        REIClientPlugin.super.registerEntries(registry);
+
+        // This probably isn't the right place to add 'recipes' for generated tools like Engineered stuff
     }
 }
